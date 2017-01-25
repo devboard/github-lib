67b81ddbcfa2c83298b2c0949bcc19a22cccb986
Miro Svrtan <miro@mirosvrtan.me>
2017-01-25 23:09:08 +0100

    Defin github user login

---
+++ spec/User/GithubUserLoginSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\Github\User;
+
+use Devboard\Github\User\GithubUserLogin;
+use PhpSpec\ObjectBehavior;
+
+class GithubUserLoginSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('octocat');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GithubUserLogin::class);
+    }
+
+    public function it_should_expose_value()
+    {
+        $this->getValue()->shouldReturn('octocat');
+    }
+
+    public function it_should_be_castable_to_string()
+    {
+        $this->__toString()->shouldReturn('octocat');
+    }
+}
---
+++ tests/User/GithubUserLoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Devboard\Github\User;
+
+use Devboard\Github\User\GithubUserLogin;
+
+/**
+ * @covers \Devboard\Github\User\GithubUserLogin
+ * @group  unit
+ */
+class GithubUserLoginTest extends \PHPUnit_Framework_TestCase
+{
+    /** @dataProvider provideUserLogins */
+    public function testItExposesValue($login)
+    {
+        $sut = new GithubUserLogin($login);
+        $this->assertEquals($login, $sut->getValue());
+    }
+
+    /** @dataProvider provideUserLogins */
+    public function testItCanBeAutoConvertedToString($login)
+    {
+        $sut = new GithubUserLogin($login);
+        $this->assertEquals($login, (string) $sut);
+    }
+
+    public function provideUserLogins()
+    {
+        return [
+            ['octocat'],
+            ['devboard'],
+        ];
+    }
+}
