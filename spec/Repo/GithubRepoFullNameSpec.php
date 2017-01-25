2aa7926fd3cf8e6b0d3d48cf9b19dffed8e2c40c
Miro Svrtan <miro@mirosvrtan.me>
2017-01-25 23:16:34 +0100

    Define github repo full name

---
+++ spec/Repo/GithubRepoFullNameSpec.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\Github\Repo;
+
+use Devboard\Github\Repo\GithubRepoFullName;
+use Devboard\Github\Repo\GithubRepoName;
+use Devboard\Github\User\GithubUserLogin;
+use PhpSpec\ObjectBehavior;
+
+class GithubRepoFullNameSpec extends ObjectBehavior
+{
+    public function let(GithubUserLogin $owner, GithubRepoName $repoName)
+    {
+        $owner->getValue()->willReturn('octocat');
+        $repoName->getValue()->willReturn('super-library');
+
+        $this->beConstructedWith($owner, $repoName);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GithubRepoFullName::class);
+    }
+
+    public function it_can_be_created_from_string()
+    {
+        $this->createFromString('octocat/super-library')->shouldReturnAnInstanceOf(GithubRepoFullName::class);
+    }
+
+    public function it_should_expose_owner_as_object(GithubUserLogin $owner)
+    {
+        $this->getOwner()->shouldReturn($owner);
+    }
+
+    public function it_should_expose_repository_name_as_object(GithubRepoName $repoName)
+    {
+        $this->getRepoName()->shouldReturn($repoName);
+    }
+
+    public function it_should_expose_value()
+    {
+        $this->getValue()->shouldReturn('octocat/super-library');
+    }
+
+    public function it_should_be_castable_to_string()
+    {
+        $this->__toString()->shouldReturn('octocat/super-library');
+    }
+}
