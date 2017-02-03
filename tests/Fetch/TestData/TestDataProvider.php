5a41be715a0fb266f7ab28cc65f38d15c4a59d7a
Miro Svrtan <miro@mirosvrtan.me>
2017-02-03 22:16:47 +0100

    Add TestDataProvider

---
+++ tests/Fetch/TestData/TestDataProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Devboard\Github\Fetch\TestData;
+
+class TestDataProvider
+{
+    private $repos = [
+        'octocat/Hello-World',
+        'octocat/Spoon-Knife',
+        'octocat/linguist',
+        'octocat/octocat.github.io',
+        'octocat/git-consortium',
+        'octocat/test-repo1',
+    ];
+
+    public function getRandomGitHubRepoData(): array
+    {
+        $repo = $this->repos[0];
+
+        return json_decode($this->loadContent($repo), true);
+    }
+
+    public function getGitHubRepoData(): array
+    {
+        $results = [];
+
+        foreach ($this->repos as $repo) {
+            $results[] = [json_decode($this->loadContent($repo), true)];
+        }
+
+        return $results;
+    }
+
+    private function loadContent(string $repo): string
+    {
+        return file_get_contents(__DIR__.'/'.$repo.'/repo.json');
+    }
+}
