05bc195f90854804d7c84e7691dc31435d8a86a4
Miro Svrtan <miro@mirosvrtan.me>
2017-02-04 16:09:20 +0100

    Add GithubCommitCommitter #41

---
+++ spec/Fetch/Commit/GithubCommitCommitterSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\Github\Fetch\Commit;
+
+use Devboard\Github\Commit\Committer\GithubCommitCommitterEmail as CommitterEmail;
+use Devboard\Github\Commit\Committer\GithubCommitCommitterName as CommitterName;
+use Devboard\Github\Commit\GithubCommitDate as CommitDate;
+use Devboard\Github\Fetch\Commit\GithubCommitCommitter;
+use Devboard\Github\Fetch\Commit\GithubCommitCommitterDetails;
+use PhpSpec\ObjectBehavior;
+
+class GithubCommitCommitterSpec extends ObjectBehavior
+{
+    public function let(
+        CommitterName $committerName,
+        CommitterEmail $committerEmail,
+        CommitDate $commitDate,
+        GithubCommitCommitterDetails $committerDetails
+    ) {
+        $this->beConstructedWith(
+            $committerName,
+            $committerEmail,
+            $commitDate,
+            $committerDetails
+        );
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GithubCommitCommitter::class);
+    }
+}
