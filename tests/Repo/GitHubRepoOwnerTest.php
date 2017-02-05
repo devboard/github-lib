e9badcb6106e00460aad58d841520192a0aa3813
Miro Svrtan <miro@mirosvrtan.me>
2017-02-05 16:44:39 +0100

    Add serialization/deserialization to GitHubRepoOwner

--- tests/Repo/GitHubRepoOwnerTest.php
+++ tests/Repo/GitHubRepoOwnerTest.php
@@ -46,6 +46,26 @@ class GitHubRepoOwnerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($siteAdmin, $sut->isSiteAdmin());
     }
 
+    /** @dataProvider provideArguments */
+    public function testSerializationAndDeserialization(
+        GitHubUserId $userId,
+        GitHubUserLogin $login,
+        GitHubUserType $githubUserType,
+        GitHubUserAvatarUrl $avatarUrl,
+        GitHubUserGravatarId $gravatarId,
+        GitHubUserHtmlUrl $htmlUrl,
+        GitHubUserApiUrl $apiUrl,
+        bool $siteAdmin
+    ) {
+        $sut = new GitHubRepoOwner(
+            $userId, $login, $githubUserType, $avatarUrl, $gravatarId, $htmlUrl, $apiUrl, $siteAdmin
+        );
+
+        $serialized = $sut->serialize();
+
+        $this->assertEquals($sut, GitHubRepoOwner::deserialize($serialized));
+    }
+
     public function provideArguments(): array
     {
         return [
