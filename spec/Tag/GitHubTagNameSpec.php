5c2683d3dc83892460235f068855e309a35a6c8e
Miro Svrtan <miro@mirosvrtan.me>
2017-03-03 20:23:20 +0100

    Add TagName #69

---
+++ spec/Tag/GitHubTagNameSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\GitHub\Tag;
+
+use Devboard\GitHub\Tag\GitHubTagName;
+use PhpSpec\ObjectBehavior;
+
+class GitHubTagNameSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('0.1.0');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GitHubTagName::class);
+    }
+
+    public function it_will_expose_value()
+    {
+        $this->getValue()->shouldReturn('0.1.0');
+    }
+
+    public function it_should_be_castable_to_string()
+    {
+        $this->__toString()->shouldReturn('0.1.0');
+    }
+}
