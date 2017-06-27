d0c4c38cfec0061a82a1c4cd05737d0d2c8c4dce
Miro Svrtan <miro@mirosvrtan.me>
2017-06-27 22:10:02 +0200

    :#104 Add GitHubInstallationAccessTokenUrl

---
+++ spec/Installation/GitHubInstallationAccessTokenUrlSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\GitHub\Installation;
+
+use Devboard\GitHub\Installation\GitHubInstallationAccessTokenUrl;
+use PhpSpec\ObjectBehavior;
+
+class GitHubInstallationAccessTokenUrlSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('https://api.github.com/installations/9001/access_tokens');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GitHubInstallationAccessTokenUrl::class);
+    }
+
+    public function it_should_expose_value()
+    {
+        $this->getValue()->shouldReturn('https://api.github.com/installations/9001/access_tokens');
+    }
+
+    public function it_should_be_castable_to_string()
+    {
+        $this->__toString()->shouldReturn('https://api.github.com/installations/9001/access_tokens');
+    }
+}
