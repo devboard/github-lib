1acedde0d826f05f69db1616aad00bccc85182b1
Miro Svrtan <miro@mirosvrtan.me>
2017-06-27 22:10:11 +0200

    :#104 Add GitHubInstallationId

---
+++ spec/Installation/GitHubInstallationIdSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\GitHub\Installation;
+
+use Devboard\GitHub\Installation\GitHubInstallationId;
+use PhpSpec\ObjectBehavior;
+
+class GitHubInstallationIdSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith(9001);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GitHubInstallationId::class);
+    }
+
+    public function it_should_expose_value()
+    {
+        $this->getValue()->shouldReturn(9001);
+    }
+
+    public function it_should_be_castable_to_string()
+    {
+        $this->__toString()->shouldReturn('9001');
+    }
+}
