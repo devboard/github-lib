6db219cfde47db83c4fe8f98839f126231a871bb
Miro Svrtan <miro@mirosvrtan.me>
2017-06-27 22:10:26 +0200

    :#104 Add GitHubInstallationRepositoriesUrl

---
+++ spec/Installation/GitHubInstallationRepositoriesUrlSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Devboard\GitHub\Installation;
+
+use Devboard\GitHub\Installation\GitHubInstallationRepositoriesUrl;
+use PhpSpec\ObjectBehavior;
+
+class GitHubInstallationRepositoriesUrlSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('https://api.github.com/installation/repositories');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(GitHubInstallationRepositoriesUrl::class);
+    }
+
+    public function it_should_expose_value()
+    {
+        $this->getValue()->shouldReturn('https://api.github.com/installation/repositories');
+    }
+
+    public function it_should_be_castable_to_string()
+    {
+        $this->__toString()->shouldReturn('https://api.github.com/installation/repositories');
+    }
+}
